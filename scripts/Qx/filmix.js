cb1cb955ca6686ea8f790c124a44393a7a064d6f
Chen Kerui <165684836+djdj554@users.noreply.github.com>
2025-03-15 20:22:51 +0800

    Update

--- scripts/Qx/filmix.js
+++ scripts/Qx/filmix.js
@@ -10,7 +10,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/appv2\.filmix\.com\.cn\/api\/v\d\/users url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/filmix.js
+^https:\/\/appv3\.filmix\.com\.cn\/api\/v1\/user\/user url script-response-body https://raw.githubusercontent.com/268326/2222/refs/heads/main/scripts/Qx/filmix.js
 
 [mitm]
 hostname = appv2.filmix.com.cn
